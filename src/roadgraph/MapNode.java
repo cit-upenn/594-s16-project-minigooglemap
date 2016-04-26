de8d72ecd5ebe6a647395c31ee9c4a7bf976f80f
qiannanling <lingqiannan@gmail.com>
2016-04-26 18:54:59 -0400

    modify roadgraph package

--- src/roadgraph/MapNode.java
+++ src/roadgraph/MapNode.java
@@ -1,12 +1,5 @@
 package roadgraph;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import geography.GeographicPoint;
-
 public class MapNode {
 	
 }
