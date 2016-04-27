1bc58b0497bb3b2730037cbc78f8421c0619f05d
qiannanling <lingqiannan@gmail.com>
2016-04-27 15:31:41 -0400

    MapGraph first ver

--- src/roadgraph/MapGraph.java
+++ src/roadgraph/MapGraph.java
@@ -1,5 +1,27 @@
 package roadgraph;
 
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import geography.GeographicPoint;
+
+/**
+ * The class that represent the graph of the study area.
+ * In the graph, the nodes are the intersections of the roads, and
+ * the edges are the roads
+ * @author Qiannan
+ *
+ */
 public class MapGraph {
 	
+	private Map<GeographicPoint, MapNode> vertexMap;
+	private Set<MapEdge> edges;
+	
+	public MapGraph() {
+		vertexMap = new HashMap<>();
+		edges = new HashSet<>();
+	}
 }
