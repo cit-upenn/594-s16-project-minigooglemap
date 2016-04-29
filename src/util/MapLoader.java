50c8ad10e66c05d321767943facca787d0359863
ZiyuChen2828 <zychen2828@gmail.com>
2016-04-29 19:58:25 -0400

    roadinfo update

--- src/util/MapLoader.java
+++ src/util/MapLoader.java
@@ -23,18 +23,16 @@ import roadgraph.MapGraph;
  * A utility class that reads various kinds of files into different 
  * graph structures.
  */
-public class MapLoader implements GraphLoader
-{
+public class MapLoader implements GraphLoader  {
 	
 	@Override
-	public void createIntersectionsFile(String roadDataFile, String intersectionsFile)
-	{
+	public void createIntersectionsFile(String roadDataFile, String intersectionsFile) {
+		
 		Collection<GeographicPoint> nodes = new HashSet<GeographicPoint>();
-        HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>> pointMap = 
-        		buildPointMapOneWay(roadDataFile);
+        HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>> pointMap = buildPointMapOneWay(roadDataFile);
+		List<GeographicPoint> intersections = findIntersections(pointMap);
 		
         // Print the intersections to the file
-		List<GeographicPoint> intersections = findIntersections(pointMap);
 		for (GeographicPoint pt : intersections) {
 			nodes.add(pt);
 		}
@@ -67,6 +65,100 @@ public class MapLoader implements GraphLoader
 			System.out.println("Exception opening intersections file " + e);
 		}
 	
+	}
+	
+	// Build the map from points to lists of lists of lines.
+	// The map returned is indexed by a GeographicPoint.  The values
+	// are lists of length two where each entry in the list is a list.
+	// The first list stores the outgoing roads while the second 
+	// stores the outgoing roads.
+	private HashMap<GeographicPoint, List<LinkedList<RoadLineInfo>>> buildPointMapOneWay(String filename){
+		BufferedReader reader = null;
+        HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>> pointMap = 
+        		new HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>>();
+		try {
+            String nextLine;
+            reader = new BufferedReader(new FileReader(filename));
+            // Read the lines out of the file and put them in a HashMap by points
+            while ((nextLine = reader.readLine()) != null) {
+            	RoadLineInfo line = splitInputString(nextLine);
+            	addToPointsMapOneWay(line, pointMap);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Problem loading dictionary file: " + filename);
+            e.printStackTrace();
+        }
+		
+		return pointMap;
+	}
+	
+	
+	// Find all the intersections. 
+	// Including dead ends, intersections between different roads
+	private List<GeographicPoint> findIntersections(HashMap<GeographicPoint,
+			List<LinkedList<RoadLineInfo>>> pointMap) {
+		// Now find the intersections.  These are roads that do not have
+		// Exactly 1 or 2 roads coming in and out, where the roads in
+		// match the roads out.
+		List<GeographicPoint> intersections = new LinkedList<GeographicPoint>();
+		for (GeographicPoint pt : pointMap.keySet()) {
+			List<LinkedList<RoadLineInfo>> roadsInAndOut = pointMap.get(pt);
+			LinkedList<RoadLineInfo> roadsOut = roadsInAndOut.get(0);
+			LinkedList<RoadLineInfo> roadsIn = roadsInAndOut.get(1);
+			
+			boolean isNode = true;
+			
+			if (roadsIn.size() == 1 && roadsOut.size() == 1) {
+				// If these are the reverse of each other, then this is
+				// and intersection (dead end)
+				if (!(roadsIn.get(0).point1.equals(roadsOut.get(0).point2) &&
+						roadsIn.get(0).point2.equals(roadsOut.get(0).point1))
+						&& roadsIn.get(0).roadName.equals(roadsOut.get(0).roadName)) {
+					isNode = false;
+				}
+			}
+			if (roadsIn.size() == 2 && roadsOut.size() == 2) {
+				// If all the road segments have the same name, 
+				// And there are two pairs of reversed nodes, then 
+				// this is not an intersection because the roads pass
+				// through.
+			
+				String name = roadsIn.get(0).roadName;
+				boolean sameName = true;
+				for (RoadLineInfo info : roadsIn) {
+					if (!info.roadName.equals(name)) {
+						sameName = false;
+					}
+				}
+				for (RoadLineInfo info : roadsOut) {
+					if (!info.roadName.equals(name)) {
+						sameName = false;
+					}
+				}
+				
+				RoadLineInfo in1 = roadsIn.get(0);
+				RoadLineInfo in2 = roadsIn.get(1);
+				RoadLineInfo out1 = roadsOut.get(0);
+				RoadLineInfo out2 = roadsOut.get(1);
+		
+				boolean passThrough = false;
+				if ((in1.isReverse(out1) && in2.isReverse(out2)) ||
+						(in1.isReverse(out2) && in2.isReverse(out1))) {
+					
+					passThrough = true;
+				} 
+				
+				if (sameName && passThrough) {
+					isNode = false;
+				} 
+
+			} 
+			if (isNode) {
+				intersections.add(pt);
+			}
+		}
+		return intersections;
 	}
 
 	
@@ -187,105 +279,6 @@ public class MapLoader implements GraphLoader
 		toReturn.add(end);
 		
 		return toReturn;
-	}
-
-
-	
-	// Find all the intersections.  Intersections are either dead ends 
-	// (1 road in and 1 road out, which are the reverse of each other)
-	// or intersections between two different roads, or where three
-	// or more segments of the same road meet.
-	private List<GeographicPoint> 
-	findIntersections(HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>> pointMap) {
-		// Now find the intersections.  These are roads that do not have
-		// Exactly 1 or 2 roads coming in and out, where the roads in
-		// match the roads out.
-		List<GeographicPoint> intersections = new LinkedList<GeographicPoint>();
-		for (GeographicPoint pt : pointMap.keySet()) {
-			List<LinkedList<RoadLineInfo>> roadsInAndOut = pointMap.get(pt);
-			LinkedList<RoadLineInfo> roadsOut = roadsInAndOut.get(0);
-			LinkedList<RoadLineInfo> roadsIn = roadsInAndOut.get(1);
-			
-			boolean isNode = true;
-			
-			if (roadsIn.size() == 1 && roadsOut.size() == 1) {
-				// If these are the reverse of each other, then this is
-				// and intersection (dead end)
-				if (!(roadsIn.get(0).point1.equals(roadsOut.get(0).point2) &&
-						roadsIn.get(0).point2.equals(roadsOut.get(0).point1))
-						&& roadsIn.get(0).roadName.equals(roadsOut.get(0).roadName)) {
-					isNode = false;
-				}
-			}
-			if (roadsIn.size() == 2 && roadsOut.size() == 2) {
-				// If all the road segments have the same name, 
-				// And there are two pairs of reversed nodes, then 
-				// this is not an intersection because the roads pass
-				// through.
-			
-				String name = roadsIn.get(0).roadName;
-				boolean sameName = true;
-				for (RoadLineInfo info : roadsIn) {
-					if (!info.roadName.equals(name)) {
-						sameName = false;
-					}
-				}
-				for (RoadLineInfo info : roadsOut) {
-					if (!info.roadName.equals(name)) {
-						sameName = false;
-					}
-				}
-				
-				RoadLineInfo in1 = roadsIn.get(0);
-				RoadLineInfo in2 = roadsIn.get(1);
-				RoadLineInfo out1 = roadsOut.get(0);
-				RoadLineInfo out2 = roadsOut.get(1);
-		
-				boolean passThrough = false;
-				if ((in1.isReverse(out1) && in2.isReverse(out2)) ||
-						(in1.isReverse(out2) && in2.isReverse(out1))) {
-					
-					passThrough = true;
-				} 
-				
-				if (sameName && passThrough) {
-					isNode = false;
-				} 
-
-			} 
-			if (isNode) {
-				intersections.add(pt);
-			}
-		}
-		return intersections;
-	}
-		
-	// Build the map from points to lists of lists of lines.
-	// The map returned is indexed by a GeographicPoint.  The values
-	// are lists of length two where each entry in the list is a list.
-	// The first list stores the outgoing roads while the second 
-	// stores the outgoing roads.
-	private HashMap<GeographicPoint, List<LinkedList<RoadLineInfo>>>
-	buildPointMapOneWay(String filename)
-	{
-		BufferedReader reader = null;
-        HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>> pointMap = 
-        		new HashMap<GeographicPoint,List<LinkedList<RoadLineInfo>>>();
-		try {
-            String nextLine;
-            reader = new BufferedReader(new FileReader(filename));
-            // Read the lines out of the file and put them in a HashMap by points
-            while ((nextLine = reader.readLine()) != null) {
-            	RoadLineInfo line = splitInputString(nextLine);
-            	addToPointsMapOneWay(line, pointMap);
-            }
-            reader.close();
-        } catch (IOException e) {
-            System.err.println("Problem loading dictionary file: " + filename);
-            e.printStackTrace();
-        }
-		
-		return pointMap;
 	}
 
 
--- src/util/RoadLineInfo.java
+++ src/util/RoadLineInfo.java
@@ -4,91 +4,80 @@ import geography.GeographicPoint;
 
 //A class to store information about the lines in the road files.
 class RoadLineInfo {
-	GeographicPoint point1;
-	GeographicPoint point2;
+	GeographicPoint point1, point2;
+	String roadName, roadType;
 	
-	String roadName;
-	String roadType;
-	
-	/** Create a new RoadLineInfo object to store information about the line 
+	/** constructor for RoadLineInfo
 	 * read from the file
-	 * @param p1 One of the points
+	 * @param p1 One point
 	 * @param p2 The other point
-	 * @param roadName The name of the road
-	 * @param roadType The type of the road
+	 * @param roadName name of the road
+	 * @param roadType type of the road
 	 */
-	RoadLineInfo(GeographicPoint p1, GeographicPoint p2, String roadName, String roadType) 
+	public RoadLineInfo(GeographicPoint p1, GeographicPoint p2, String roadName, String roadType) 
 	{
-		point1 = p1;
-		point2 = p2;
+		this.point1 = p1;
+		this.point2 = p2;
 		this.roadName = roadName;
 		this.roadType = roadType;
 	}
-	
-	
-	/** Get the other point from this roadLineInfo */
-	public GeographicPoint getOtherPoint(GeographicPoint pt)
-	{
-		if (pt == null) throw new IllegalArgumentException();
-		if (pt.equals(point1)) {
-			return point2;
-		}
-		else if (pt.equals(point2)) {
-			return point1;
-		}
-		else throw new IllegalArgumentException();
-	}
-	
-	/** Two RoadLineInfo objects are considered equal if they have the same
-	 * two points and the same roadName and roadType.
+
+	/**
+	 * This method determine whether two RoadLineInfo obj are the same
 	 */
 	public boolean equals(Object o)
 	{
-		if (o == null || !(o instanceof RoadLineInfo))
-		{
+		if (o == null) {
+			return false;
+		}
+		if (!(o instanceof RoadLineInfo)) {
 			return false;
 		}
 		RoadLineInfo info = (RoadLineInfo)o;
 		return info.point1.equals(this.point1) && info.point2.equals(this.point2)  &&
 				info.roadType.equals(this.roadType) && info.roadName.equals(this.roadName);
-				
 	}
 	
-	/** Calculate the hashCode based on the hashCodes of the two points
+	/** Get the other point */
+	public GeographicPoint getOtherPoint(GeographicPoint pt) {
+		if (pt == null || (!pt.equals(point1) && !pt.equals(point2))){
+			throw new IllegalArgumentException();
+		}
+		return pt.equals(point1)? point2 : point1;
+	}
+	
+	/** Return a reversed road */
+	public RoadLineInfo getReverseCopy()
+	{	
+		RoadLineInfo reverse = new RoadLineInfo(this.point2, this.point1, this.roadName, this.roadType);
+		return reverse;
+	}
+	
+	
+	/** Calculate the hashCode based on the hashCodes of the two GeographicPoint
 	 * @return The hashcode for this object.
 	 */
 	public int hashCode()
-	{
+	{	
 		return point1.hashCode() + point2.hashCode();
-		
 	}
 	
-	/** Returns whether these segments are part of the same road in terms of
-	 * road name and road type.
-	 * @param info The RoadLineInfo to compare against.
-	 * @return true if these represent the same road, false otherwise.
+	/** Check if these segments are part of the same road 
+	 * @param roadInfo The RoadLineInfo to be checked.
+	 * @return true/false based on whether they are in the same road or not.
 	 */
-	public boolean sameRoad(RoadLineInfo info)
-	{
-		return info.roadName.equals(this.roadName) && info.roadType.equals(this.roadType);
+	public boolean sameRoad(RoadLineInfo roadInfo){
+		return roadInfo.roadName.equals(this.roadName) && roadInfo.roadType.equals(this.roadType);
 	}
 	
-	/** Return a copy of this LineInfo in the other direction */
-	public RoadLineInfo getReverseCopy()
-	{
-		return new RoadLineInfo(this.point2, this.point1, this.roadName, this.roadType);
-	}
-	
-	/** Return true if this road is the same segment as other, but in reverse
-	 *   Otherwise return false.
-	 */
+
 	public boolean isReverse(RoadLineInfo other)
 	{
 		return this.point1.equals(other.point2) && this.point2.equals(other.point1) &&
 				this.roadName.equals(other.roadName) && this.roadType.equals(other.roadType);
 	}
 	
-	/** Return the string representation of this LineInfo. */
+	/* toString */
 	public String toString()
 	{
 		return this.point1 + " " + this.point2 + " " + this.roadName + " " + this.roadType;
