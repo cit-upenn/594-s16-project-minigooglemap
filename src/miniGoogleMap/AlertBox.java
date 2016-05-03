196ca16750056eea6e0094ad86dc5bfcc277895c
XiaofanDou <670750304@qq.com>
2016-05-03 10:54:26 -0400

    java doc added for alertBox class

--- src/miniGoogleMap/AlertBox.java
+++ src/miniGoogleMap/AlertBox.java
@@ -1,17 +1,25 @@
 package miniGoogleMap;
 
-import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * A class to manage alert box.
+ * @author xiaofandou
+ *
+ */
 public class AlertBox {
 	
+	/**
+	 * display an alert box with given tile and message.
+	 * @param title
+	 * @param message
+	 */
 	public static void display(String title, String message) {
 		
 		Stage window = new Stage();
