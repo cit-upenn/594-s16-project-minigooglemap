0ff18e0193166786a9dfabe9cc1556237b822f5e
XiaofanDou <670750304@qq.com>
2016-05-03 11:08:53 -0400

    java doc added for LabelManager.java

--- src/miniGoogleMap/LabelManager.java
+++ src/miniGoogleMap/LabelManager.java
@@ -2,8 +2,18 @@ package miniGoogleMap;
 
 import javafx.scene.control.Label;
 
+/**
+ * A class for manage label
+ * @author xiaofandou
+ *
+ */
 public class LabelManager {
 
+	/**
+	 * set the given label given content.
+	 * @param label
+	 * @param s
+	 */
 	public static void setLabelText(Label label, String s) {
 		label.setText(s);
 	}
